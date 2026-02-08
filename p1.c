4a8e15efb1ce3b8d63e8224577e98c9ff4f0f2be
pinakmanvar <pinakmanvark@gmail.com>
2026-02-08 09:45:14 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+
+{
+  int a,b;
+  printf("enter the value of A:     ");
+  scanf("%d",&a);
+  printf("enter the value of B:     ");
+  scanf("%d",&b);
+
+printf("the addition of A and B is ");
+  printf("%d \n",a+b);
+printf("the substraction of A and B is ");
+  printf("%d \n",a-b);
+printf("the product of A and B is ");
+  printf("%d \n",a*b);
+printf("the division of A and B is ");
+  printf("%d \n",a/b);
+printf("the remainder of A and B is ");
+  printf("%d \n",a%b);
+  
+  return 0;
+}
+
